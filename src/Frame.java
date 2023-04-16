d0ed6c6861e741f6e102e4bc8f05ab9473e02191
tanmayhaldar27 <130904109+tanmayhaldar27@users.noreply.github.com>
2023-04-16 17:24:44 +0530

    Add files via upload

---
+++ src/Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    Frame(){
+        this.setTitle("SnakeGame");
+        this.add(new Panel());
+        this.setVisible(true);
+        this.setResizable(false);
+        this.pack();
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //calling the frame class
+        new Frame();
+    }
+}
